3465af40510ac46f22b3a2c9957b358c7b48944d
Карина Новикова <karinanovikova@MacBook-Pro-Karina.local>
2022-03-29 22:58:05 +0300

    create posts

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,53 @@
 <template>
-    <div><button>;alalalalla    </button></div>
+    <div class="app">
+        <post-form/>
+        <post-list/>
+    </div>
 </template>
 
 <script>
+import PostForm from "./components/PostForm";
+import PostList from "./components/PostList";
+
+export default {
+    components: {
+        PostList, PostForm
+    },
+    data() {
+        return {
+            posts: [
+                {id: 1, title: 'Javascript', body: 'Описание поста'},
+                {id: 2, title: 'Javascript 2', body: 'Описание поста 2'},
+                {id: 3, title: 'Javascript 3', body: 'Описание поста 3'},
+            ],
+            title: '',
+            body: ''
+        }
+    },
+    methods: {
+       createPost(event){
+           const newPost = {
+            id: Date.now(),
+            title: this.title,
+            body: this.body,
+        }
+        this.posts.push(newPost);
+        this.title= '';
+        this.body = '';
+       },
+       
+    }
+}
 
 </script>
 
-<style>
-
+<style scoped>
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+.app{
+    padding: 20px;
+}
 </style>
---
+++ src/components/PostForm.vue
@@ -0,0 +1,50 @@
+<template>
+    <div>
+        <form @submit.prevent>
+                <h4>Создание поста</h4>
+                <input 
+                    :value="title"
+                    @input="title = $event.target.value"
+                    class="input" 
+                    type="text" 
+                    placeholder="Название"
+                >
+                <input 
+                    :value="body"
+                    @input="body = $event.target.value"
+                    class="input" 
+                    type="text" 
+                    placeholder="Описание"
+                >
+                <button 
+                    class="btn" 
+                    @click="createPost"
+                >Создать</button>
+            </form>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+form{
+    display: flex;
+    flex-direction: column;
+}
+.input{
+    width: 100%;
+    border: 1px solid teal;
+    padding: 10px 15px;
+    margin-top: 15px;
+}
+.btn{
+    margin-top: 15px;
+    align-self: flex-end;
+    padding: 10px 15px;
+    background: none;
+    color: teal;
+    border: 1px solid teal;
+}
+</style>
