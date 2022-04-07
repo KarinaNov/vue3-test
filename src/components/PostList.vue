ebf6dcc3ed0352f60e6a64bc436020cb5e1ac6b9
Карина Новикова <karinanovikova@MacBook-Pro-Karina.local>
2022-04-07 23:04:19 +0300

    add pagination

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,12 +1,14 @@
 <template>
     <div v-if="posts.length > 0">
         <h3>Список постов</h3>
-        <post-item
-            v-for="post in posts"
-            :key="post.id"
-            :post="post"
-            @remove="$emit('remove', post)"
-        />
+        <transition-group name="user-list">
+            <post-item
+                v-for="post in posts"
+                :key="post.id"
+                :post="post"
+                @remove="$emit('remove', post)"
+            />
+        </transition-group>
     </div>
     <h2 v-else>Список постов пуст</h2>
 </template>
@@ -27,5 +29,18 @@ export default {
 
 
 <style scoped>
-
+.user-list-item {
+  display: inline-block;
+  margin-right: 10px;
+}
+.user-list-enter-active, .user-list-leave-active {
+  transition: all .5s;
+}
+.user-list-enter, .user-list-leave-to /* .list-leave-active до версии 2.1.8 */ {
+  opacity: 0;
+  transform: translateX(130px);
+}
+.user-list-move{
+    transition: transform .4s;
+}
 </style>
